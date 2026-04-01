e5ddd2d30d2a7ae97b395fb8f983b292fb1b8095
2546309 <2546309@cegeplimoilou.ca>
2026-04-01 10:26:24 -0400

    ajout des classes  circuitBuilder et Circuitapp

--- src/app/CircuitBuilder.java
+++ src/app/CircuitBuilder.java
@@ -1,4 +1,37 @@
 package app;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import electronique.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CircuitBuilder {
+    public CircuitBuilder(){}
+
+    public List<Composant> construireCircuit(String cheminFichier){
+        List<Composant> listeComposant = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            // on lit le fichier
+            JsonNode donneescircuit = mapper.readTree(new File(cheminFichier));
+            // On boucle sur chaque noeud
+            for(JsonNode noeudComposant : donneescircuit){
+                // Extraction des donnees
+                String type = noeudComposant.get("type").asText();
+
+            }
+
+        }catch (IOException e) {
+
+            System.err.println("Erreur de lecture : " + e.getMessage());
+
+        }
+        return listeComposant;
+    }
+    public List<Composant> lireComposant(JsonNode node){
+
+    }
 }
