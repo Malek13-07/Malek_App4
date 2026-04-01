5bca4837c2e087e0ef8ee0471e53c4858e79e39a
2546309 <2546309@cegeplimoilou.ca>
2026-04-01 12:31:02 -0400

    ajout de la méthode lireComposant

--- src/app/CircuitBuilder.java
+++ src/app/CircuitBuilder.java
@@ -9,10 +9,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CircuitBuilder {
-    public CircuitBuilder() {
-    }
+    public CircuitBuilder() {}
 
     public List<Composant> construireCircuit(String cheminFichier) {
+
         List<Composant> listeComposant = new ArrayList<>();
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -22,7 +22,26 @@ public class CircuitBuilder {
             for (JsonNode noeudComposant : donneescircuit) {
                 // Extraction des donnees
                 String type = noeudComposant.get("type").asText();
-                double Valeur = noeudComposant.get("valeur").asDouble();
+
+                // 4. Création selon le type
+                if ("resistance".equals(type)) {
+
+                    double valeur = noeudComposant.get("valeur").asDouble();
+                    listeComposant.add(new Resistance("resisitance",valeur));
+
+                } else if ("serie".equals(type)) {
+
+                    List<Composant> sousComposants = lireComposants(noeudComposant.get("composants"));
+                    listeComposant.add(new CircuitSerie("serie", sousComposants));
+
+                } else if ("parallele".equals(type)) {
+
+                    List<Composant> sousComposants = lireComposants(noeudComposant.get("composants"));
+                    listeComposant.add(new CircuitParallele("parallele",sousComposants));
+
+                } else {
+                    throw new IllegalArgumentException("Type inconnu : " + type);
+                }
             }
 
         } catch (IOException e) {
@@ -33,80 +52,39 @@ public class CircuitBuilder {
         return listeComposant;
     }
 
-    public List<Composant> lireComposant(JsonNode node) {
+    private List<Composant> lireComposants(JsonNode node) {
 
-    }
-}
-    List<Composant> listeComposants = new ArrayList<>();
-    ObjectMapper mapper = new ObjectMapper();
+        List<Composant> liste = new ArrayList<>();
 
-        try {
-        // 1. Lire le fichier JSON
-        JsonNode donneesCircuit = mapper.readTree(new File(cheminFichier));
+        for (JsonNode noeud : node) {
 
-        // 2. Boucle sur chaque noeud
-        for (JsonNode noeudComposant : donneesCircuit) {
+            String type = noeud.get("type").asText();
 
-            // 3. Extraction des données
-            String type = noeudComposant.get("type").asText();
-
-            // 4. Création selon le type
             if ("resistance".equals(type)) {
 
-                double valeur = noeudComposant.get("valeur").asDouble();
-                listeComposants.add(new Resistance(valeur));
+                double valeur = noeud.get("valeur").asDouble();
+                liste.add(new Resistance("resistance",valeur));
 
             } else if ("serie".equals(type)) {
 
-                List<Composant> sousComposants = lireComposants(noeudComposant.get("composants"));
-                listeComposants.add(new CircuitSerie(sousComposants));
+                List<Composant> sous = lireComposants(noeud.get("composants"));
+                liste.add(new CircuitSerie("serie",sous));
 
             } else if ("parallele".equals(type)) {
 
-                List<Composant> sousComposants = lireComposants(noeudComposant.get("composants"));
-                listeComposants.add(new CircuitParallele(sousComposants));
+                List<Composant> sous = lireComposants(noeud.get("composants"));
+                liste.add(new CircuitParallele("parallele",sous));
 
             } else {
                 throw new IllegalArgumentException("Type inconnu : " + type);
             }
         }
 
-    } catch (IOException e) {
-        System.err.println("Erreur de lecture : " + e.getMessage());
+        return liste;
     }
-
-        return listeComposants;
 }
 
-// 🔁 Méthode récursive (comme Ecosysteme du prof)
-private List<Composant> lireComposants(JsonNode node) {
 
-    List<Composant> liste = new ArrayList<>();
 
-    for (JsonNode noeud : node) {
 
-        String type = noeud.get("type").asText();
-
-        if ("resistance".equals(type)) {
-
-            double valeur = noeud.get("valeur").asDouble();
-            liste.add(new Resistance(valeur));
-
-        } else if ("serie".equals(type)) {
-
-            List<Composant> sous = lireComposants(noeud.get("composants"));
-            liste.add(new CircuitSerie(sous));
-
-        } else if ("parallele".equals(type)) {
-
-            List<Composant> sous = lireComposants(noeud.get("composants"));
-            liste.add(new CircuitParallele(sous));
-
-        } else {
-            throw new IllegalArgumentException("Type inconnu : " + type);
-        }
-    }
-
-    return liste;
-}
 
